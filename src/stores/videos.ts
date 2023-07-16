56dea32e437e11dea720dc309b213487b47ccee7
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-07-16 14:32:58 +0200

    Updated video store

--- src/stores/videos.ts
+++ src/stores/videos.ts
@@ -2,8 +2,8 @@ import { defineStore } from 'pinia';
 
 import berlin_romance_1 from '@/assets/videos/Berlin_Romance_1.mov';
 import berlin_romance_2 from '@/assets/videos/Berlin_Romance_2.mov';
-import poster1 from '@/assets/images/video_1.jpg';
-import poster2 from '@/assets/images/video_2.jpg';
+import poster1 from '@/assets/videos/video_1_poster.jpg';
+import poster2 from '@/assets/videos/video_2_poster.jpg';
 
 export const useVideoStore = defineStore({
     id: 'videoStore',
