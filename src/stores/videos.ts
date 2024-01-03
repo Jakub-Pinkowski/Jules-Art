7384645873bdda7603bba0051e576d04f471a95b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-01-03 20:02:48 +0100

    Update video imports and add new video and poster

--- src/stores/videos.ts
+++ src/stores/videos.ts
@@ -5,13 +5,14 @@ import berlin_romance_2 from '@/assets/videos/Berlin_Romance_2.mov'
 import jane from '@/assets/videos/Jane.mov'
 import despair from '@/assets/videos/Despair.mov'
 import wizard_tales from '@/assets/videos/Wizards_Tales.mov'
-import paper from '@/assets/videos/Paper.mp4'
+import guardian_of_klowledge from '@/assets/videos/guardian_of_knowledge.mp4'
 
 import poster1 from '@/assets/videos/Berlin_Romance_1.jpg'
 import poster2 from '@/assets/videos/Berlin_Romance_2.jpg'
 import poster3 from '@/assets/videos/Jane_poster.jpg'
 import poster4 from '@/assets/videos/Despair.jpg'
 import poster5 from '@/assets/videos/Wizards_Tales.jpg'
+import poster6 from '@/assets/videos/guardian_of_knowledge.jpg'
 
 export const useVideoStore = defineStore({
     id: 'videoStore',
@@ -39,15 +40,15 @@ export const useVideoStore = defineStore({
                 poster: poster4,
             },
             {
-                name: 'Wizard\'s Tales',
+                name: "Wizard's Tales",
                 src: wizard_tales,
                 poster: poster5,
             },
             {
                 name: 'Guardian of Knowledge',
-                src: paper,
-                poster: poster5,
-            }
+                src: guardian_of_klowledge,
+                poster: poster6,
+            },
         ],
     }),
 })
