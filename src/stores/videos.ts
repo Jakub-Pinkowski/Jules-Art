e9cf33dc1ecc90b1a03fcaccf256d17de8d5b699
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-01-03 19:20:28 +0100

    Add new videos and posters

--- src/stores/videos.ts
+++ src/stores/videos.ts
@@ -3,9 +3,15 @@ import { defineStore } from 'pinia'
 import berlin_romance_1 from '@/assets/videos/Berlin_Romance_1.mov'
 import berlin_romance_2 from '@/assets/videos/Berlin_Romance_2.mov'
 import jane from '@/assets/videos/Jane.mov'
+import despair from '@/assets/videos/Despair.mov'
+import wizard_tales from '@/assets/videos/Wizards_Tales.mov'
+import paper from '@/assets/videos/Paper.mp4'
+
 import poster1 from '@/assets/videos/Berlin_Romance_1.jpg'
 import poster2 from '@/assets/videos/Berlin_Romance_2.jpg'
 import poster3 from '@/assets/videos/Jane_poster.jpg'
+import poster4 from '@/assets/videos/Despair.jpg'
+import poster5 from '@/assets/videos/Wizards_Tales.jpg'
 
 export const useVideoStore = defineStore({
     id: 'videoStore',
@@ -27,6 +33,21 @@ export const useVideoStore = defineStore({
                 src: jane,
                 poster: poster3,
             },
+            {
+                name: 'Despair',
+                src: despair,
+                poster: poster4,
+            },
+            {
+                name: 'Wizard\'s Tales',
+                src: wizard_tales,
+                poster: poster5,
+            },
+            {
+                name: 'Guardian of Knowledge',
+                src: paper,
+                poster: poster5,
+            }
         ],
     }),
 })
